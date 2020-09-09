897e3d8e8b51e062db3ec6e6f56c53b101e83882
yafeunteun <yfe.github@protonmail.com>
2020-09-09 11:57:32 +0200

    remove legacy code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from preprocessor import Preprocessor
 from environment import Environment
 from monkey import MonkeyAgent
-from cnn_agent import CNNAgent
+
 
 ## Constants
 width = 80
@@ -14,43 +14,10 @@ def softmax(x):
     return np.array(x)/sum(x)
 
 def crossover(parentA, parentB):
-    offspring = CNNAgent(num_actions, width, height, None, None)
-
-    for idx, layer in enumerate(parentB.model.model.layers):
-        w = layer.get_weights()
-        if len(w) > 0:
-            pos_0 = np.random.randint(np.size(w[0]))
-            pos_1 = np.random.randint(np.size(w[1]))
-            shape_0 = w[0].shape
-            shape_1 = w[1].shape
-            
-            w_in = np.concatenate((
-                w[0].ravel()[:pos_0],
-                parentA.model.model.layers[idx].get_weights()[0].ravel()[pos_0:]
-            ))
-            w_out = np.concatenate((
-                w[1].ravel()[:pos_1],
-                parentA.model.model.layers[idx].get_weights()[1].ravel()[pos_1:]
-            ))
-            
-            offspring.model.model.layers[idx].set_weights([
-                w_in.reshape(shape_0),
-                w_out.reshape(shape_1)
-            ])
-        return offspring
-                
+    raise NotImplementedError
+                    
 def mutate(agent, pmut=.01):
-    for idx, layer in enumerate(agent.model.model.layers):
-        w = layer.get_weights()
-        if len(w) > 0:
-            for x in np.nditer(w[0], op_flags=['readwrite']):
-                if np.random.random() < pmut:
-                    x[...] = np.random.normal(0, 1)
-            for x in np.nditer(w[1], op_flags=['readwrite']):
-                if np.random.random() < pmut:
-                    x[...] = np.random.normal(0, 1)
-
-            agent.model.model.layers[idx].set_weights([w[0], w[1]])
+    raise NotImplementedError
 
 def play_generation(population, env, preprocessor):
     gen_count = 0
@@ -108,11 +75,11 @@ def play(agent, env, preprocessor):
 def main():
     # Initialize key objects: environment, agent and preprocessor
     env = Environment("127.0.0.1", 8765)
-    agent = CNNAgent(num_actions, width, height, None, None)
+    agent = MonkeyAgent(num_actions, width, height, None, None)
 
     preprocessor = Preprocessor(width, height)
     #play(agent, env, preprocessor)
-    play_generation([CNNAgent(num_actions, width, height, None, None) for _ in range(10)], env, preprocessor)
+    play_generation([MonkeyAgent(num_actions, width, height, None, None) for _ in range(10)], env, preprocessor)
 
 if __name__ == "__main__":
     main()
